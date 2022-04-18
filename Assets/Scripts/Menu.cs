7871a0e0deb05d0aa2e214d5416ead842c99ef5b
Harshit Khandelwal <hk6848@gmail.com>
2022-04-19 00:45:48 +0530

    UI updated

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour {
-
-  
+public class Menu : MonoBehaviour
+{
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-
 
     public void QuitGame()
     {
